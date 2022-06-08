699ace21d7734a10844a0897ec9807ddec1fd67a
lincoln anderson <lincolnanderson@me.com>
2022-06-08 15:00:53 -0500

    small change to pagehome

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -6,13 +6,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import ServiceCatalog from '@/components/ServiceCatalog.vue'
-import AddNewServiceButton from '@/components/AddNewServiceButton.vue'
 
 export default defineComponent({
   name: 'PageHome',
   components: {
     ServiceCatalog,
-    AddNewServiceButton,
   },
 })
 </script>
